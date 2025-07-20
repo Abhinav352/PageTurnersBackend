43b26078d021dcead16c0b90ca028299c579e485
Abhinav352 <abhinav0298@gmail.com>
2025-07-20 18:56:08 +0530

    Project Creation

---
+++ src/main/java/com/example/PageTurners/PageTurnersApplication.java
@@ -0,0 +1,13 @@
+package com.example.PageTurners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PageTurnersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PageTurnersApplication.class, args);
+	}
+
+}
